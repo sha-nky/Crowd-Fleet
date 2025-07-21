cc3a8105a1a871d6dc8f9ece023b1813e2359c29
Shanky <shashanksharma03.07@gmail.com>
2025-07-21 23:15:29 +0530

    Folder structure made and react & tailwindcss initialised

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='h-screen w-full flex justify-center bg-blue-400'>
+      <p className='text-7xl text-white font-bold'>SMART CROWD</p>
+    </div>
+  )
+}
